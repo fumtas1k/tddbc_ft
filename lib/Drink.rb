d252631e1f65107153784062a968290d217d5df3
fumtas1k <fumta01a7@gmail.com>
2022-03-12 13:33:26 +0900

    add:Drinkクラス作成

---
+++ lib/Drink.rb
@@ -0,0 +1,24 @@
+class Drink
+  atter_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def inspect
+    name
+  end
+
+  def hash
+    name.hash
+  end
+
+  def eql?(other)
+    other.instance_of?(Drink) && name.eql?(other.name)
+  end
+
+  def ==(other)
+    other.is_a?(Drink) && name == other.name
+  end
+end
