ef5cce5383ef938d870f6b07b17a30b2d683054a
fumtas1k <fumta01a7@gmail.com>
2022-03-13 16:13:44 +0900

    add:moneyクラス作成。まだ、CashManager修正していない

--- test/drink_test.rb
+++ test/drink_test.rb
@@ -16,9 +16,11 @@ class DrinkTest < Minitest::Test
   def test_eql_when_same_drink_and_same_class
     assert @coke.eql?(Drink.new(:coke, 120))
   end
+
   def test_eql_when_different_drink_and_same_class
     refute @coke.eql?(Drink.new(:water, 100))
   end
+
   def test_eql_when_different_drink_and_different_class
     refute @coke.eql?(:coke)
   end
@@ -26,9 +28,11 @@ class DrinkTest < Minitest::Test
   def test_equal_when_same_drink_and_same_class
     assert @coke == Drink.new(:coke, 120)
   end
+
   def test_equal_when_different_drink_and_same_class
     refute @coke == Drink.new(:water, 100)
   end
+
   def test_eql_when_different_drink_and_different_class
     refute @coke == :coke
   end
